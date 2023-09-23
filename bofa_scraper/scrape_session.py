47062a52112fab9b2d731c14e46b1847ce41bd7f
nluongo <nluongo@gmail.com>
2023-09-23 11:31:47 -0500

    Increase wait and logging

--- bofa_scraper/scrape_session.py
+++ bofa_scraper/scrape_session.py
@@ -37,23 +37,28 @@ class ScrapeSession:
 		out: list[Transaction] = []
 		row: WebElement
 		table_id = 'transactions'
+		print('Here?')
+		if len(self.driver.find_elements(By.ID, table_id)) == 0:
+			Log.log('No transaction table found')    
+			return self
 		table = self.driver.find_element(By.ID, table_id)
 		body = table.find_element(By.TAG_NAME, "tbody")
 		rows = body.find_elements(By.TAG_NAME, "tr")
-        Log.log('%d transactions found.' % (len(rows))) 
+		Log.log('%d transactions found.' % (len(rows)))
 		for i, row in enumerate(rows):
 			cell = row.find_element(By.TAG_NAME, "td")
 			expander = cell.find_element(By.TAG_NAME, "a")
 			self.driver.execute_script("arguments[0].scrollIntoView();", expander)
 			WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((expander)))
-			time.sleep(0.1)
+			time.sleep(1)
 			expander.click()
-			time.sleep(0.1)
+			time.sleep(1)
 			transaction = Transaction()
 			transaction.amount = float(row.find_element(By.CLASS_NAME, "trans-amount-cell").text.replace(",","").replace("$",""))
 			transaction.balance = float(row.find_element(By.CLASS_NAME, "trans-balance-cell").text.replace(",","").replace("$",""))
 			transaction.date = row.find_element(By.CLASS_NAME, "trans-date-cell").text
 			transaction.desc = row.find_element(By.CLASS_NAME, "expand-trans-from-desc").text
+			#WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "second-expanded-cell")))
 			transaction.trans_date = row.find_element(By.CLASS_NAME, "second-expanded-cell").text
 
 			out.append(transaction)
